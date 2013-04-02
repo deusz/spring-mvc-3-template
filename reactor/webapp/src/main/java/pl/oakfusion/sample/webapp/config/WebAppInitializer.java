941f24c8153746a73d41f8b29b9924a2d128d3b0
lszydlo <szydler@gmail.com>
2013-04-02 09:56:56 +0200

    add application initializer

---
+++ reactor/webapp/src/main/java/pl/oakfusion/sample/webapp/config/WebAppInitializer.java
@@ -0,0 +1,49 @@
+package pl.oakfusion.sample.webapp.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import java.util.Set;
+
+@SuppressWarnings("UnusedDeclaration")
+/**
+ * Implementations of this SPI will be detected automatically by {@link
+ * org.springframework.web.SpringServletContainerInitializer}
+ */
+public class WebAppInitializer implements WebApplicationInitializer {
+	@Override
+	public void onStartup(final ServletContext servletContext) throws ServletException {
+
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.setConfigLocation("pl.oakfusion.sample.webapp.config");
+
+		/*FilterRegistration.Dynamic securityFilter = servletContext.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain"));
+		securityFilter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "*//*");
+		FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("characterEncodingFilter", new CharacterEncodingFilter());
+		characterEncodingFilter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+		characterEncodingFilter.setInitParameter("encoding", "UTF-8");
+		characterEncodingFilter.setInitParameter("forceEncoding", "true");
+*/
+
+		servletContext.addListener(new ContextLoaderListener(context));
+		servletContext.setInitParameter("defaultHtmlEscape", "true");
+
+		DispatcherServlet servlet = new DispatcherServlet();
+		// no explicit configuration reference here: everything is configured in the root container for simplicity
+		servlet.setContextConfigLocation("");
+
+		ServletRegistration.Dynamic appServlet = servletContext.addServlet("appServlet", servlet);
+		appServlet.setLoadOnStartup(1);
+		appServlet.setAsyncSupported(true);
+
+		Set<String> mappingConflicts = appServlet.addMapping("/");
+		if (!mappingConflicts.isEmpty()) {
+			throw new IllegalStateException("'appServlet' cannot be mapped to '/' under Tomcat versions <= 7.0.14");
+		}
+	}
+}
